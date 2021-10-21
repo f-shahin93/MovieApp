46a4350fc73dd214be386ff7a3f6001d5d223844
F_Shahin <f.shahin.kh@gmail.com>
2021-10-21 23:11:42 +0330

    remove unused code.

--- app/src/main/java/com/shahin/movieapp/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/shahin/movieapp/ui/splash/SplashActivity.kt
@@ -4,20 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import androidx.lifecycle.ViewModelProvider
 import com.shahin.movieapp.R
-import com.shahin.movieapp.ui.movieList.MainActivity
-import com.shahin.movieapp.viewModel.MainViewModel
+import com.shahin.movieapp.ui.MainActivity
 
 class SplashActivity : AppCompatActivity() {
-    private var mMainViewModel: MainViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_splash)
-        mMainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        mMainViewModel!!.getMoviesList()
+
         Handler().postDelayed({
             val intent = Intent(this@SplashActivity, MainActivity::class.java)
             startActivity(intent)
